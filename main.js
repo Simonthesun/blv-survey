77d5eaa904ba14e8a5e39f73a781f56930e195c6
Simon Sun <simonthesun@yahoo.com>
2022-04-19 15:37:49 -0700

    closed survey

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ $(document).ready(() => {
 });
 
 function redirect() {
-    let links = ["https://forms.gle/WPmGUA2yWdu5Aucy8", "https://forms.gle/amqmnBim7tQFDZZUA", "https://forms.gle/cuFAEfAdV8W56E7Y8", "https://forms.gle/QaGpo9XcBS9pqBph9", "https://forms.gle/Qj68rfKreKLnkwbF8", "https://forms.gle/jKGbNDjMCnpuYHVH8", "https://forms.gle/mmPuV5pLLVBuhxw26", "https://forms.gle/T3LUAMvymuJ4bJrP6", "https://forms.gle/58gu3RFF9jDKdxoa8", "https://forms.gle/7mhNzYhZzJyUGtZi6"];
+    /* let links = ["https://forms.gle/WPmGUA2yWdu5Aucy8", "https://forms.gle/amqmnBim7tQFDZZUA", "https://forms.gle/cuFAEfAdV8W56E7Y8", "https://forms.gle/QaGpo9XcBS9pqBph9", "https://forms.gle/Qj68rfKreKLnkwbF8", "https://forms.gle/jKGbNDjMCnpuYHVH8", "https://forms.gle/mmPuV5pLLVBuhxw26", "https://forms.gle/T3LUAMvymuJ4bJrP6", "https://forms.gle/58gu3RFF9jDKdxoa8", "https://forms.gle/7mhNzYhZzJyUGtZi6"];
     let cookie = getCookieIfExists();
 
     if (cookie == null) {
@@ -14,7 +14,9 @@ function redirect() {
         var url = links[n]
     } else {
         var url = links[cookie];
-    }
+    } */
+
+    var url = "https://forms.gle/e9QTUkNzMUPceWAw8"; //notify sign up link
 
     location.assign(url);
 }
